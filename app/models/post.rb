dca474fc52e2b2ccb852bd8de89e5a549a9043aa
github username <github email address>
2020-10-21 12:36:17 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,7 @@
 class Post < ActiveRecord::Base
-<<<<<<< HEAD
     #post will show just title and desctiption combined 
     def post_summary
         self.title + " - " + self.description
     end
 
 end
-=======
-  
-  def post_summary
-  self.title + " - " + self.description
-  end
-
-end
->>>>>>> 3fa89ca57934014821368ce61e97f142c1b3ae3c
